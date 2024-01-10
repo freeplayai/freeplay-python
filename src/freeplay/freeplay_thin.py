bb9634ae4f2fe7165327c2257661dd846e134525
BrianNewsom <brian.l.newsom@gmail.com>
2024-01-10 15:44:08 -0700

    Revert "Temporarily roll-back thin SDK for next publish. To be reverted after publish."
    This reverts commit 6e138a8ebf9720eb2f01d9b6fcb8d4ae29656f85.

--- src/freeplay/freeplay_thin.py
+++ src/freeplay/freeplay_thin.py
@@ -1,7 +1,62 @@
-from .completions import PromptTemplates
-from .errors import FreeplayConfigurationError
-from .record import DefaultRecordProcessor
-from .support import (CallSupport)
+import json
+import uuid
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Tuple
+
+from .completions import PromptTemplates, ChatMessage, OpenAIFunctionCall, PromptTemplateWithMetadata
+from .errors import FreeplayConfigurationError, FreeplayClientError
+from .flavors import Flavor
+from .model import InputVariables
+from .support import CallSupport
+from .llm_parameters import LLMParameters
+from .record import DefaultRecordProcessor, RecordCallFields
+from .utils import bind_template_variables
+
+
+@dataclass
+class Session:
+    session_id: str
+
+
+@dataclass
+class PromptInfo:
+    prompt_template_id: str
+    prompt_template_version_id: str
+    template_name: str
+    environment: str
+    variables: InputVariables
+    model_parameters: LLMParameters
+    provider: str
+    model: str
+    flavor_name: str
+
+
+@dataclass
+class CallInfo:
+    provider: str
+    model: str
+    start_time: float
+    end_time: float
+    model_parameters: LLMParameters
+    test_run_id: Optional[str] = None
+
+
+@dataclass
+class ResponseInfo:
+    is_complete: bool
+    function_call_response: Optional[OpenAIFunctionCall] = None
+    prompt_tokens: Optional[int] = None
+    response_tokens: Optional[int] = None
+
+
+@dataclass
+class RecordPayload:
+    all_messages: List[ChatMessage]
+    session_id: str
+
+    prompt_info: PromptInfo
+    call_info: CallInfo
+    response_info: ResponseInfo
 
 
 class FreeplayThin:
@@ -13,9 +68,99 @@ class FreeplayThin:
         if not freeplay_api_key or not freeplay_api_key.strip():
             raise FreeplayConfigurationError("Freeplay API key not set. It must be set to the Freeplay API.")
 
-        self.call_support = CallSupport(freeplay_api_key, api_base, DefaultRecordProcessor(freeplay_api_key, api_base))
+        self.call_support = CallSupport(freeplay_api_key, api_base,
+                                        DefaultRecordProcessor(freeplay_api_key, api_base))
         self.freeplay_api_key = freeplay_api_key
         self.api_base = api_base
 
+    # noinspection PyMethodMayBeStatic
+    def create_session(self) -> Session:
+        return Session(session_id=str(uuid.uuid4()))
+
     def get_prompts(self, project_id: str, tag: str) -> PromptTemplates:
         return self.call_support.get_prompts(project_id=project_id, tag=tag)
+
+    def get_prompt(
+            self,
+            project_id: str,
+            template_name: str,
+            environment: str,
+            variables: InputVariables
+    ) -> Tuple[PromptInfo, List[ChatMessage]]:
+        prompt_template = self.call_support.get_prompt(
+            project_id=project_id,
+            template_name=template_name,
+            environment=environment
+        )
+
+        messages: List[Dict[str, str]] = json.loads(prompt_template.content)
+        bound_messages: List[ChatMessage] = [
+            {
+                "content": bind_template_variables(message['content'], variables),
+                "role": message['role']
+            } for message in messages
+        ]
+
+        params = prompt_template.get_params()
+        model = params.pop('model')
+
+        if not prompt_template.flavor_name:
+            raise FreeplayConfigurationError(
+                "Flavor must be configured in the Freeplay UI. Unable to fulfill request.")
+
+        flavor = Flavor.get_by_name(prompt_template.flavor_name)
+
+        prompt_info = PromptInfo(
+            prompt_template_id=prompt_template.prompt_template_id,
+            prompt_template_version_id=prompt_template.prompt_template_version_id,
+            template_name=prompt_template.name,
+            environment=environment,
+            variables=variables,
+            model_parameters=params,
+            provider=flavor.provider,
+            model=model,
+            flavor_name=prompt_template.flavor_name
+        )
+
+        return prompt_info, bound_messages
+
+    # noinspection PyMethodMayBeStatic
+    def format(self, flavor_name: str, messages: List[ChatMessage]) -> str | List[ChatMessage]:
+        flavor = Flavor.get_by_name(flavor_name)
+        return flavor.to_llm_syntax(messages)
+
+    def record_call(self, record_payload: RecordPayload) -> None:
+        if len(record_payload.all_messages) < 1:
+            raise FreeplayClientError("Messages list must have at least one message. "
+                                      "The last message should be the current response.")
+
+        completion = record_payload.all_messages[-1]
+        history_as_string = json.dumps(record_payload.all_messages[0:-1])
+
+        template = PromptTemplateWithMetadata(
+            prompt_template_id=record_payload.prompt_info.prompt_template_id,
+            prompt_template_version_id=record_payload.prompt_info.prompt_template_version_id,
+            name=record_payload.prompt_info.template_name,
+            content=history_as_string,
+            flavor_name=record_payload.prompt_info.flavor_name,
+            params=record_payload.prompt_info.model_parameters
+        )
+
+        self.call_support.record_processor.record_call(
+            RecordCallFields(
+                formatted_prompt=history_as_string,
+                completion_content=completion['content'],
+                completion_is_complete=record_payload.response_info.is_complete,
+                start=record_payload.call_info.start_time,
+                end=record_payload.call_info.end_time,
+                session_id=record_payload.session_id,
+                target_template=template,
+                variables=record_payload.prompt_info.variables,
+                tag=record_payload.prompt_info.environment,
+                test_run_id=record_payload.call_info.test_run_id,
+                model=record_payload.call_info.model,
+                provider=record_payload.prompt_info.provider,
+                llm_parameters=record_payload.call_info.model_parameters,
+                record_format_type=None  # This is deprecated and unused in the API
+            )
+        )
---
+++ tests/test_freeplay_thin.py
@@ -0,0 +1,169 @@
+import json
+import time
+from unittest import TestCase
+from uuid import uuid4
+
+import responses
+
+from freeplay.errors import (FreeplayClientError,
+                             FreeplayConfigurationError)
+from freeplay.freeplay_thin import FreeplayThin, CallInfo, ResponseInfo, RecordPayload
+
+
+class TestFreeplay(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.freeplay_api_key = "freeplay_api_key"
+        self.openai_api_key = "openai_api_key"
+        self.api_base = "http://localhost:9091/api"
+        self.project_id = str(uuid4())
+        self.project_version_id = str(uuid4())
+        self.prompt_template_version_id = self.project_version_id
+        self.prompt_template_id_1 = str(uuid4())
+        self.record_url = f'{self.api_base}/v1/record'
+        self.tag = 'test-tag'
+
+        self.freeplay_thin = FreeplayThin(
+            freeplay_api_key=self.freeplay_api_key,
+            api_base=self.api_base
+        )
+
+    @responses.activate
+    def test_single_prompt_get_and_record(self) -> None:
+        self.__mock_freeplay_apis()
+
+        prompt_info, messages = self.freeplay_thin.get_prompt(
+            project_id=self.project_id,
+            template_name="my-chat-prompt",
+            environment=self.tag,
+            variables={"name": "Sparkles", "question": "Why isn't my door working"}
+        )
+
+        ready_for_llm = self.freeplay_thin.format(prompt_info.flavor_name, messages)
+
+        session = self.freeplay_thin.create_session()
+        start = time.time()
+        end = start + 5
+        openai_response = 'This is the response from Anthropic'
+        messages.append({'role': 'Assistant', 'content': ('%s' % openai_response)})
+
+        call_info = CallInfo(
+            provider=prompt_info.provider,
+            model=prompt_info.model,
+            start_time=start,
+            end_time=end,
+            model_parameters=prompt_info.model_parameters
+        )
+        response_info = ResponseInfo(
+            is_complete=True
+        )
+
+        self.freeplay_thin.record_call(
+            RecordPayload(
+                all_messages=messages,
+                session_id=session.session_id,
+                prompt_info=prompt_info,
+                call_info=call_info,
+                response_info=response_info
+            )
+        )
+
+        self.assertTrue(str(ready_for_llm).startswith("\n\nHuman: System message"))
+
+        record_api_request = responses.calls[1].request
+        recorded_body_dom = json.loads(record_api_request.body)
+
+        self.assertEqual(True, recorded_body_dom['is_complete'])
+        self.assertEqual(self.project_version_id, recorded_body_dom['project_version_id'])
+        self.assertEqual(self.prompt_template_id_1, recorded_body_dom['prompt_template_id'])
+        self.assertIsNotNone(recorded_body_dom['start_time'])
+        self.assertIsNotNone(recorded_body_dom['end_time'])
+        self.assertEqual(self.tag, recorded_body_dom['tag'])
+        self.assertEqual(
+            '[{"content": "System message", "role": "system"}, '
+            '{"content": "How may I help you, Sparkles?", "role": "Assistant"}, '
+            '{"content": "Why isn\'t my door working", "role": "user"}]',
+            recorded_body_dom['prompt_content']
+        )
+        self.assertEqual(openai_response, recorded_body_dom['return_content'])
+        self.assertEqual('anthropic', recorded_body_dom['provider'])
+        self.assertEqual(0.7, recorded_body_dom['llm_parameters']['temperature'])
+        self.assertEqual(50, recorded_body_dom['llm_parameters']['max_tokens_to_sample'])
+
+    @responses.activate
+    def test_auth_error(self) -> None:
+        responses.get(
+            url=f'{self.api_base}/projects/{self.project_id}/templates/all/{self.tag}',
+            status=401,
+            body=self.__get_templates_response()
+        )
+
+        freeplay_thin = FreeplayThin(
+            freeplay_api_key="not-the-key",
+            api_base=self.api_base,
+        )
+
+        with self.assertRaisesRegex(FreeplayClientError, "Error getting prompt templates \\[401\\]"):
+            freeplay_thin.get_prompt(
+                project_id=self.project_id,
+                template_name="my-chat-prompt",
+                environment=self.tag,
+                variables={}
+            )
+
+    @responses.activate
+    def test_template_not_found(self) -> None:
+        self.__mock_freeplay_apis()
+        with self.assertRaisesRegex(
+                FreeplayConfigurationError,
+                'Could not find template with name "invalid-template-id"'
+        ):
+            self.freeplay_thin.get_prompt(
+                project_id=self.project_id,
+                template_name="invalid-template-id",
+                environment=self.tag,
+                variables={}
+            )
+
+    def __mock_freeplay_apis(self) -> None:
+        responses.get(
+            url=f'{self.api_base}/projects/{self.project_id}/templates/all/{self.tag}',
+            status=200,
+            body=self.__get_templates_response()
+        )
+        self.__mock_record_api()
+
+    def __mock_record_api(self) -> None:
+        responses.post(
+            url=self.record_url,
+            status=201,
+            content_type='application/json'
+        )
+
+    def __get_templates_response(self) -> str:
+        return json.dumps({
+            'templates': [
+                {
+                    'content': json.dumps([{
+                        "role": "system",
+                        "content": "System message"
+                    }, {
+                        "role": "Assistant",
+                        "content": "How may I help you, {{name}}?"
+                    }, {
+                        "role": "user",
+                        "content": "{{question}}"
+                    }]),
+                    'name': 'my-chat-prompt',
+                    'project_version_id': self.project_version_id,
+                    'prompt_template_version_id': self.prompt_template_version_id,
+                    'prompt_template_id': self.prompt_template_id_1,
+                    'flavor_name': 'anthropic_chat',
+                    'params': {
+                        'model': 'claude-2.1',
+                        'max_tokens_to_sample': 50,
+                        'temperature': 0.7
+                    }
+                },
+            ]
+        })
